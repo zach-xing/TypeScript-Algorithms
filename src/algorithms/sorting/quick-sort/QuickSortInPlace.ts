d8a7862f02a28c2cb206dd93361c9c61caf48910
Zeekg <3193373997@qq.com>
2022-07-13 09:30:27 +0800

    feat:quick sork in place

--- src/algorithms/sorting/quick-sort/QuickSortInPlace.ts
+++ src/algorithms/sorting/quick-sort/QuickSortInPlace.ts
@@ -9,7 +9,26 @@ export default class QuickSortInPlace extends Sort {
   ) {
     const array = recursiveCall ? originalArray : [...originalArray];
     const partitionArray = (lowIndex: number, highIndex: number) => {
-      
+      const tmp = array[highIndex];
+      this.callbacks.visitingCallback(tmp);
+      let flagIdx = lowIndex;
+      for (let i = lowIndex; i < highIndex; i++) {
+        if (this.comparator.lessThan(array[i], tmp)) {
+          [array[i], array[flagIdx]] = [array[flagIdx], array[i]];
+          flagIdx++;
+        }
+      }
+      [array[highIndex], array[flagIdx]] = [array[flagIdx], array[highIndex]];
+
+      return flagIdx;
     };
+
+    if (inputLowIndex < inputHighIndex) {
+      const mid = partitionArray(inputLowIndex, inputHighIndex);
+      this.sort(array, inputLowIndex, mid - 1, true);
+      this.sort(array, mid + 1, inputHighIndex, true);
+    }
+
+    return array;
   }
 }
